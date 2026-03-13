accb37c0c37ca56c2ba836ab87311cf412bfc7e6
ahror-dev <xudayqulovahror@gmail.com>
2026-03-13 14:56:26 +0500

    Initial Commit: Toliq Versiyani Yozib Tugatdim

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"buffio"
+	"bufio"
 	"io"
 	"math/rand/v2"
 	"os"
@@ -14,7 +14,7 @@ import (
 
 
 // qiyinchilik darajasi
-type Difficulty int
+type DifficultyLevel int
 
 const (
 	Easy DifficultyLevel = iota
@@ -70,7 +70,7 @@ func (hs *HighScores) Update(level DifficultyLevel, attempts int) {
 
 // yangi baland natijalarning toplari
 func (hs *HighScores) Best(level DifficultyLevel) (int, bool) {
-	score, exists := scores[level]
+	score, exists := hs.scores[level]
 	return score, exists	
 }
 
@@ -80,10 +80,10 @@ func (hs *HighScores) Print(w io.Writer) {
 	fmt.Println("Eng yaxshi natijalar: ")
 	for _, level := range AllLevels(){
 		if score, exists := hs.scores[level]; exists {
-			fmt.Fprintf(w, "  %-8s : %d urinish\n", level, score)
+			fmt.Fprintf(w, " \n %-8s : %d urinish\n", level, score)
 
 		}else {
-			fmt.Fprintf(w, "Oyin hali oynalmagan!", level)
+			fmt.Fprintf(w, "\n%s Oyin hali oynalmagan!", level)
 		}
 	}
 }
@@ -138,11 +138,11 @@ func Hint(secret, guess, remaining int) string {
 			return fmt.Sprintf("Oxirgi urinish! Son %d atrofida... ", (secret/10)*10+5)
 		}
 	}
-	return map[HintLevel]string {
+	return map[HintLevel]string{
 		HintFire: "🔥 Juda Yaqin! Bir qadam qoldi.",
-		HintHot: "♨️ Yaqinlashyapsan."
-		HintWarm: "😐 To'g'ri yo'ldasan"
-		HintIce: "❄️ Ancha uzoqda"
+		HintHot: "♨️ Yaqinlashyapsan.",
+		HintWarm: "😐 To'g'ri yo'ldasan",
+		HintIce: "❄️ Ancha uzoqda",
 		
 	}[hintLevel(secret, guess)]
 }
@@ -151,40 +151,208 @@ func Hint(secret, guess, remaining int) string {
 
 
 // O`quvchi qism
-type Reade struct {
-	r *buffio.Reader
+type Reader struct {
+	r *bufio.Reader
 }
 
 
 func NewReader(input io.Reader) *Reader {
-	return &Reader {
-		r: buffio.NewReader(input)
-	}
+	return &Reader {r: bufio.NewReader(input)}
 }
 
+
 func (rd *Reader) ReaderLine() (string, error) {
-	line, err := rd.r.ReadString("\n")
-	return strings TrimSpace(line), err 
-} 
+	line, err := rd.r.ReadString('\n')
+	return strings.TrimSpace(line), err 
+}
 
 
 
-func (rd *Reader) ReadInt(min, max int) (int, err) {
-	line, err := rd.ReadLine()
-	iff err != nil {
+func (rd *Reader) ReadInt(min, max int) (int, error) {
+	line, err := rd.ReaderLine()
+	if err != nil {
 		return 0, err
 	}
 
 	n, err := strconv.Atoi(line)
-	iff err != nil {
-		return 0, fmt.Errorf("invalid: %q (expected)", line, min, max)
+	if err != nil {
+		return 0, fmt.Errorf("invalid: %q (expected %d-%d)", line, min, max)
+	}
+	if n < min || n > max {
+		return 0, fmt.Errorf("number out of range: %d (expected %d-%d)", n, min, max)
 	}
 	return n, nil
 }
 
 
 func (rd *Reader) ReadConfirm() bool {
-	line, _ := rd.ReadLine()
+	line, _ := rd.ReaderLine()
 	line = strings.ToLower(line)
 	return line == "ha" || line == "h" || line == "yes" || line == "y"
+}
+
+
+
+
+// ROUND
+
+// RoundResult Structi
+type RoundResult struct {
+	Won bool
+	Secret int
+	Attempts int
+	Elapsed time.Duration
+	Level DifficultyLevel
+
+}
+
+
+// Round Structi
+type Round struct {
+	secret int
+	level DifficultyLevel
+	reader *Reader
+	writer io.Writer
+
+}
+
+
+
+
+func NewRound(secret int, level DifficultyLevel, r *Reader, w io.Writer) *Round {
+	return &Round{secret: secret, level: level, reader: r, writer: w}
+}
+
+
+
+
+
+func (r *Round) Play() RoundResult {
+	fmt.Fprintf(r.writer, "Oyin boshlandi! (1-100 oraligidagi sonni toping)\n\n")
+
+	start := time.Now()
+	maxChances := r.level.Chances()
+
+	for attempts := 1; attempts <= maxChances; attempts++ {
+		remaining := maxChances - attempts + 1
+		fmt.Fprintf(r.writer, "%d/%d urinishlaringiz qoldi!\n", remaining, maxChances)
+
+
+		guess, err := r.reader.ReadInt(1, 100)
+
+		if err != nil {
+			fmt.Fprintln(r.writer, "Iltimos 1-100 oraligidagi butun sonlarni yozing!")
+			attempts--
+			continue
+		}
+
+		if guess == r.secret {
+			elapsed := time.Since(start).Round(time.Second)
+			fmt.Fprintf(r.writer, "\n🎉 To'g'ri! Son %d edi\n", r.secret)
+			fmt.Fprintf(r.writer, "%d urinishda topdingiz(%v)\n", attempts, elapsed)
+			return RoundResult{Won: true,Secret: r.secret,	Attempts: attempts,Elapsed: elapsed,Level: r.level}
+		}
+
+		if guess < r.secret {
+			fmt.Fprintf(r.writer, "%d - bu son maxfiy sondan kichik!\n", guess)
+		}else {
+			fmt.Fprintf(r.writer, "%d - bu son maxfiy sondan katta\n", guess)
+		}
+
+		fmt.Fprintln(r.writer, Hint(r.secret, guess, remaining-1))
+		fmt.Fprintln(r.writer)
+
+	}
+
+	elapsed := time.Since(start).Round(time.Second)
+	fmt.Fprintf(r.writer, "\n💔 Urinishlaringiz tugadi, Togri son - %d. (%v)\n", r.secret, elapsed)
+	return RoundResult{Won: false, Secret: r.secret, Attempts: maxChances, Elapsed: elapsed, Level: r.level}
+
+}
+
+
+// O`YIN
+type Game struct {
+	reader *Reader
+	writer io.Writer
+	scores *HighScores
+}
+
+
+
+func NewGame(input io.Reader, output io.Writer) *Game {
+	return &Game {
+		reader: NewReader(input),
+		writer: output,
+		scores: NewHighScores(),
+
+
+	}
+}
+
+func (g *Game) Run() {
+	g.printWelcome()
+
+
+	for {
+		level, err := g.selectDifficulty()
+		if err != nil {
+			continue
+		}
+	
+
+		secret := rand.IntN(100) + 1
+		result := NewRound(secret, level, 	g.reader, g.writer).Play() 
+
+		if result.Won {
+			g.scores.Update(result.Level, result.Attempts)
+		}
+		g.scores.Print(g.writer)
+
+		fmt.Fprint(g.writer, "\nYana oynamoqchimisiz? (ha/yoq): ")
+
+		if !g.reader.ReadConfirm() {
+			break
+		}
+
+		fmt.Fprintln(g.writer, "\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+	}
+
+	fmt.Fprintln(g.writer, "Korishguncha ")
+}
+
+func (g *Game) selectDifficulty() (DifficultyLevel, error) {
+	fmt.Fprintln(g.writer, "Qiyinchilik Darajasini Tanlang(sonlarda 1/2/3)")
+	for _, level := 	range AllLevels() {
+		fmt.Fprintf(g.writer, "  %d. %-8s (%d urinish)\n", int(level)+1, level, level.Chances())
+	}
+
+	n, err := g.reader.ReadInt(1,3)
+	if err != nil {
+		fmt.Fprintln(g.writer, "⚠️  1, 2 yoki 3 ni kiriting.")
+		return 0, err
+	}
+
+	level := DifficultyLevel(n-1)
+	fmt.Fprintf(g.writer, "%s tanlandi, %d urinish\n", level, level.Chances())
+	return level, nil
+
+}
+
+
+
+func (g *Game) printWelcome() {
+	fmt.Fprintln(g.writer, "╔══════════════════════════════════════╗")
+	fmt.Fprintln(g.writer, "║      NUMBER GUESSING GAME             ║")
+	fmt.Fprintln(g.writer, "╚══════════════════════════════════════╝")
+	fmt.Fprintln(g.writer, "\n📋 QOIDALAR:")
+	fmt.Fprintln(g.writer, "   1. Kompyuter 1–100 orasida son tanlaydi")
+	fmt.Fprintln(g.writer, "   2. Qiyinchilik darajasini tanlaysiz")
+	fmt.Fprintln(g.writer, "   3. O`yindan zavqlaning!")
+	fmt.Fprintln(g.writer, "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+	fmt.Fprintln(g.writer, "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+}
+
+func main() {
+	NewGame(os.Stdin, os.Stdout).Run()
 }
